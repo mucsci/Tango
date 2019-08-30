b58a4c82cefd1e1840614dcf979363d639ad90e1
William Killian <william.killian@gmail.com>
2019-08-30 08:42:23 -0400

    Added hotfix for universal shell redirection

--- config.py
+++ config.py
@@ -29,11 +29,11 @@ class Config:
     LOGLEVEL = logging.DEBUG
 
     # Courselabs directory. Must be created before starting Tango
-    COURSELABS = "/home/autolab/data/tango/submissions"
+    COURSELABS = "/home/autolab/mu-autolab/data/tango/submissions"
 
     # Directory within each courselab where Tango will copy the output
     # for jobs of that courselab
-    OUTPUT_FOLDER = "/home/autolab/data/tango/output"
+    OUTPUT_FOLDER = "output"
 
     # VMMS to use. Must be set to a VMMS implemented in vmms/ before
     # starting Tango.  Options are: "localDocker", "distDocker",
@@ -127,7 +127,7 @@ class Config:
     ######
     # Part 4: Settings for shared memory
     #
-    USE_REDIS = True
+    USE_REDIS = False
     REDIS_HOSTNAME = "127.0.0.1"
     REDIS_PORT = 6379
 
--- vmms/tashiSSH.py
+++ vmms/tashiSSH.py
@@ -278,7 +278,7 @@ class TashiSSH:
         self.log.debug("runJob: Running job on VM %s" % domain_name)
         # Setting ulimits for VM and running job
         runcmd = "/usr/bin/time --output=time.out autodriver -u %d -f %d -t \
-            %d -o %d autolab &> output" % (config.Config.VM_ULIMIT_USER_PROC,
+            %d -o %d autolab > output 2>&1" % (config.Config.VM_ULIMIT_USER_PROC,
                                            config.Config.VM_ULIMIT_FILE_SIZE,
                                            runTimeout,
                                            config.Config.MAX_OUTPUT_FILE_SIZE)
